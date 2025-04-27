53c392fac44349fe10a627c2047401481ee07c5d
Marco Antônio de Oliveira Rocha <marcoantoniooliveirarocha3@gmail.com>
2025-04-27 08:54:47 -0300

    Update logicaSuperTrunfo.c

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -1,43 +1,82 @@
 #include <stdio.h>
+#include <string.h>
 
-// Desafio Super Trunfo - Países
-// Tema 2 - Comparação das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de comparação de cartas de cidades. 
-// Siga os comentários para implementar cada parte do desafio.
+typedef struct {
+    char estado[50];
+    char cidade[50];
+    int populacao;
+    float area;
+    float pib;
+    int pontosTuristicos;
+} Carta;
+
+void cadastrarCarta(Carta *carta) {
+    printf("\n--- Cadastro de Carta ---\n");
+    printf("Estado: ");
+    scanf(" %[^\n]", carta->estado);
+    printf("Cidade: ");
+    scanf(" %[^\n]", carta->cidade);
+    printf("Populacao: ");
+    scanf("%d", &carta->populacao);
+    printf("Area (km²): ");
+    scanf("%f", &carta->area);
+    printf("PIB (em bilhões): ");
+    scanf("%f", &carta->pib);
+    printf("Número de pontos turísticos: ");
+    scanf("%d", &carta->pontosTuristicos);
+}
+
+void compararCartas(Carta c1, Carta c2, int atributo) {
+    printf("\n--- Resultado da Comparacao ---\n");
+    switch (atributo) {
+        case 1: // População
+            if (c1.populacao > c2.populacao)
+                printf("%s venceu com maior populacao!\n", c1.cidade);
+            else
+                printf("%s venceu com maior populacao!\n", c2.cidade);
+            break;
+        case 2: // Área
+            if (c1.area > c2.area)
+                printf("%s venceu com maior area!\n", c1.cidade);
+            else
+                printf("%s venceu com maior area!\n", c2.cidade);
+            break;
+        case 3: // PIB
+            if (c1.pib > c2.pib)
+                printf("%s venceu com maior PIB!\n", c1.cidade);
+            else
+                printf("%s venceu com maior PIB!\n", c2.cidade);
+            break;
+        case 4: // Pontos Turísticos
+            if (c1.pontosTuristicos > c2.pontosTuristicos)
+                printf("%s venceu com mais pontos turísticos!\n", c1.cidade);
+            else
+                printf("%s venceu com mais pontos turísticos!\n", c2.cidade);
+            break;
+        default:
+            printf("Atributo inválido!\n");
+    }
+}
 
 int main() {
-    // Definição das variáveis para armazenar as propriedades das cidades
-    // Você pode utilizar o código do primeiro desafio
+    Carta carta1, carta2;
+    int atributo;
 
-    
-    // Cadastro das Cartas:
-    // Implemente a lógica para solicitar ao usuário que insira os dados das cidades
-    // utilizando a função scanf para capturar as entradas.
-    // utilize o código do primeiro desafio
+    printf("Cadastro da primeira carta:\n");
+    cadastrarCarta(&carta1);
 
-    // Exemplo:
-    // printf("Digite o código da cidade: ");
-    // scanf("%s", codigo);
-    // 
-    // (Repita para cada propriedade)
+    printf("\nCadastro da segunda carta:\n");
+    cadastrarCarta(&carta2);
 
-    // Comparação de Cartas:
-    // Desenvolva a lógica de comparação entre duas cartas.
-    // Utilize estruturas de decisão como if, if-else para comparar atributos como população, área, PIB, etc.
+    printf("\nEscolha o atributo para comparar:\n");
+    printf("1 - População\n");
+    printf("2 - Área\n");
+    printf("3 - PIB\n");
+    printf("4 - Pontos Turísticos\n");
+    printf("Digite o número do atributo: ");
+    scanf("%d", &atributo);
 
-    // Exemplo:
-    // if (populacaoA > populacaoB) {
-    //     printf("Cidade 1 tem maior população.\n");
-    // } else {
-    //     printf("Cidade 2 tem maior população.\n");
-    // }
-
-    // Exibição dos Resultados:
-    // Após realizar as comparações, exiba os resultados para o usuário.
-    // Certifique-se de que o sistema mostre claramente qual carta venceu e com base em qual atributo.
-
-    // Exemplo:
-    // printf("A cidade vencedora é: %s\n", cidadeVencedora);
+    compararCartas(carta1, carta2, atributo);
 
     return 0;
 }
